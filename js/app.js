1ce4fd91f6cb270f831a0c30bd9bbfa1af27d8b1
Manuuel <manuzumu@gmail.com>
2014-08-02 16:17:50 -0600

    Cambios en el cambio de contrasena
    Falta q oculte las alertas

--- js/app.js
+++ js/app.js
@@ -1,122 +1,133 @@
-(function () {
+(function() {
 
-	var confApp = function($routeProvider){
+  var confApp = function($routeProvider) {
 
-		$routeProvider.when("/",{
-			controller: "signupController",
-			templateUrl: "views/login.html"
-		}).when("/documentos",{
-			controller: "documentosController",
-			templateUrl: "views/documentos.html"
-		}).when("/visualizarDocumentos",{
-			controller: "visualizarDocumentosController",
-			templateUrl: "views/visualizarDocumentos.html"
-		}).when("/blog",{
-			controller: "blogController",
-			templateUrl: "views/blog.html"
-		}).when("/secciones",{
-			controller: "seccionesController",
-			templateUrl: "views/secciones.html"
-		}).when("/configuracion",{
-			controller: "FormCtrl",
-			templateUrl: "views/configuracion.html"
-		}).when("/foro",{
-			controller: "foroController",
-			templateUrl: "views/foros.html"
-		}).when("/foro-topic",{
-			controller: "foroController",
-			templateUrl: "views/foro-topic.html"
-		}).when("/editar",{
-			controller: "editarController",
-			templateUrl: "views/editar.html"
-		});
-	}
+    $routeProvider.when("/", {
+      controller: "signupController",
+      templateUrl: "views/login.html"
+    }).when("/documentos", {
+      controller: "documentosController",
+      templateUrl: "views/documentos.html"
+    }).when("/visualizarDocumentos", {
+      controller: "visualizarDocumentosController",
+      templateUrl: "views/visualizarDocumentos.html"
+    }).when("/blog", {
+      controller: "blogController",
+      templateUrl: "views/blog.html"
+    }).when("/secciones", {
+      controller: "seccionesController",
+      templateUrl: "views/secciones.html"
+    }).when("/configuracion", {
+      controller: "FormCtrl",
+      templateUrl: "views/configuracion.html"
+    }).when("/foro", {
+      controller: "foroController",
+      templateUrl: "views/foros.html"
+    }).when("/foro-topic", {
+      controller: "foroController",
+      templateUrl: "views/foro-topic.html"
+    }).when("/editar", {
+      controller: "editarController",
+      templateUrl: "views/editar.html"
+    });
+  }
 
-	var app = angular.module('module',['ngRoute','mgcrea.ngStrap','ui.bootstrap']).config(confApp);
+  var app = angular.module('module', ['ngRoute', 'mgcrea.ngStrap', 'ui.bootstrap']).config(confApp);
 
-	app.run(function ($rootScope, $location) {
+  app.run(function($rootScope, $location) {
 
-		$rootScope.roles = [{
-			'page': 'secciones',
-			'roles': ['profesor', 'estudiante', 'asistente','administrador']
-		},
-		{
-			'page': 'blog',
-			'roles': ['profesor', 'estudiante']
-		},
-		{
-			'page': 'documentos',
-			'roles': ['estudiante', 'profesor']
-		},
-		{
-			'page': 'foro',
-			'roles': ['estudiante', 'profesor']
-		},
-		{
-			'page': 'configuracion',
-			'roles': ['administrador']
-		}
-		];
+    $rootScope.roles = [{
+      'page': 'secciones',
+      'roles': ['profesor', 'estudiante', 'asistente', 'administrador']
+    }, {
+      'page': 'blog',
+      'roles': ['profesor', 'estudiante']
+    }, {
+      'page': 'documentos',
+      'roles': ['estudiante', 'profesor']
+    }, {
+      'page': 'foro',
+      'roles': ['estudiante', 'profesor']
+    }, {
+      'page': 'configuracion',
+      'roles': ['administrador']
+    }];
 
 
 
-		$rootScope.$on('$routeChangeStart', function (event) {
-			var $rScope = this;
-			if ($location.$$path == "/") {
-				$rootScope.showheader = 1;
-			}
-			else {
+    $rootScope.$on('$routeChangeStart', function(event) {
+      var $rScope = this;
+      if ($location.$$path == "/") {
+        $rootScope.showheader = 1;
+      } else {
 
-				$rootScope.showheader = 0;
-				if ($rootScope.usuarioLogueado == null) {
-					$location.url('/');
-				}
-				else {
-					var posicion = -1;
-					for (var i = 0; i < $rootScope.roles.length; i++) {
-						if ('/'+$rootScope.roles[i].page == $location.$$path) {
-							posicion = i;
-						}
-					}
-					if (posicion > -1) {
+        $rootScope.showheader = 0;
+        if ($rootScope.usuarioLogueado == null) {
+          $location.url('/');
+        } else {
+          var posicion = -1;
+          for (var i = 0; i < $rootScope.roles.length; i++) {
+            if ('/' + $rootScope.roles[i].page == $location.$$path) {
+              posicion = i;
+            }
+          }
+          if (posicion > -1) {
 
-						console.log($rootScope.usuarioLogueado.rol, $rootScope.roles[posicion].roles);
-						if ($rootScope.roles[posicion].roles.indexOf($rootScope.usuarioLogueado.rol) == -1) {
-							$location.url('/');
-						}
-					}
-				}
+            console.log($rootScope.usuarioLogueado.rol, $rootScope.roles[posicion].roles);
+            if ($rootScope.roles[posicion].roles.indexOf($rootScope.usuarioLogueado.rol) == -1) {
+              $location.url('/');
+            }
+          }
+        }
 
-			}
+      }
 
-		});
-		$rootScope.usuarioLogueado = null;
+    });
+    $rootScope.usuarioLogueado = null;
 
 
-	});
+  });
 
-app.directive('moduleHeader', function ($route) {
+  app.directive('moduleHeader', function($route) {
 
-	return {
-		restrict: 'E',
-		templateUrl: 'header.html'
-	};
+    return {
+      restrict: 'E',
+      templateUrl: 'header.html'
+    };
 
-});
+  });
 
-app.directive('moduleFooter', function ($route) {
+  app.directive('moduleFooter', function($route) {
 
-	return {
-		restrict: 'E',
-		templateUrl: 'footer.html'
-	};
+    return {
+      restrict: 'E',
+      templateUrl: 'footer.html'
+    };
 
-});
+  });
 
-app.controller('indexController', ['$scope', function($scope) {
+  app.directive('match', function() {
+    return {
+      require: 'ngModel',
+      restrict: 'A',
+      scope: {
+        match: '='
+      },
+      link: function(scope, elem, attrs, ctrl) {
+        scope.$watch(function() {
+          return (ctrl.$pristine && angular.isUndefined(ctrl.$modelValue)) || scope.match === ctrl.$modelValue;
+        }, function(currentValue) {
+          ctrl.$setValidity('match', currentValue);
+        });
+      }
+    };
+  });
 
-}]);
+  app.controller('indexController', ['$scope',
+    function($scope) {
 
+    }
+  ]);
 
 
 
@@ -130,6 +141,3 @@ app.controller('indexController', ['$scope', function($scope) {
 
 
 ();
-
-
-
