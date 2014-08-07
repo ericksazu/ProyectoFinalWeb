bdd45b000c1b98139527900f45017f1c052254d8
Erick Sanchez <ericksazu@gmail.com>
2014-08-06 20:23:56 -0600

    BLOG

--- js/Controllers/blogController.js
+++ js/Controllers/blogController.js
@@ -2,24 +2,35 @@ angular.module('module').controller('blogController', function($scope, $http) {
 
     $scope.datos = [];
     $scope.datos1 = [];
-    $scope.datos2 = [];
     $scope.documentos = [];
 
-    $http.get('data/usuario2.json').success(function(data) {
+    $http.get('data/usuario.json').success(function(data) {
         $scope.datos = data;
     });
 
-    $http.get('data/news.json').success(function(data) {
-        $scope.datos1 = data;
-    });
-
     $http.get('data/published.json').success(function(data) {
-        $scope.datos2 = data;
+        $scope.datos1 = data;
     });
 
     $http.get('data/documentos.json').success(function(data) {
         $scope.documentos = data;
     });
+
+    $scope.nuevaPublicacion = function() {
+
+        $scope.getpublishedtitle = document.getElementById('idPublished').value;
+        $scope.getpublishedmessage = document.getElementById('textPublished').value;
+
+        if ($scope.getpublishedtitle != '') {
+            $scope.datos1.$scope.title.push($scope.getpublishedtitle);
+            $scope.datos1.$scope.published.push($scope.getpublishedmessage);
+        } else {
+            alert('Digite todos los espacios');
+        }
+
+        $scope.getpublishedtitle = '';
+        $scope.getpublishedmessage = '';
+    };
 
     $scope.buscar = function() {
         alert($scope.documentos[0].titulo);
