1f97ac885a44be3ecedbd18a3a766b8764432810
Erick Sanchez <ericksazu@gmail.com>
2014-08-07 17:55:27 -0600

    Nueva publicación en Blog

--- js/Controllers/blogController.js
+++ js/Controllers/blogController.js
@@ -1,48 +1,55 @@
 angular.module('module').controller('blogController', function($scope, $http) {
 
-    $scope.datos = [];
-    $scope.datos1 = [];
-    $scope.documentos = [];
+  $scope.datos = [];
+  $scope.publicaciones = [];
+  $scope.documentos = [];
+  $scope.date = new Date();
 
-    $http.get('data/usuario.json').success(function(data) {
-        $scope.datos = data;
-    });
+  $http.get('data/usuario.json').success(function(data) {
+    $scope.datos = data;
+  });
 
-    $http.get('data/published.json').success(function(data) {
-        $scope.datos1 = data;
-    });
+  $http.get('data/published.json').success(function(data) {
+    $scope.publicaciones = data;
+  });
 
-    $http.get('data/documentos.json').success(function(data) {
-        $scope.documentos = data;
-    });
+  $http.get('data/documentos.json').success(function(data) {
+    $scope.documentos = data;
+  });
 
-    $scope.nuevaPublicacion = function() {
+  $scope.nuevaPublicacion = function() {
 
-        $scope.getpublishedtitle = document.getElementById('idPublished').value;
-        $scope.getpublishedmessage = document.getElementById('textPublished').value;
+    $scope.getpublishedtitle = $scope.tituloPublicacion;
+    $scope.getpublishedmessage = $scope.contenidoPublicacion;
 
-        if ($scope.getpublishedtitle != '') {
-            $scope.datos1.$scope.title.push($scope.getpublishedtitle);
-            $scope.datos1.$scope.published.push($scope.getpublishedmessage);
-        } else {
-            alert('Digite todos los espacios');
-        }
+    if($scope.getpublishedtitle != '') {
 
-        $scope.getpublishedtitle = '';
-        $scope.getpublishedmessage = '';
-    };
+      $scope.publicaciones.unshift({"title":$scope.getpublishedtitle, "fecha":$scope.date ,"published":$scope.getpublishedmessage});
 
-    $scope.buscar = function() {
-        alert($scope.documentos[0].titulo);
-    };
+    }else{
+      alert('Digite todos los espacios');
+    }
 
-    $scope.marca = function(index, votos) {
-        console.log(index, votos);
-        if (index <= votos) {
-            return "glyphicon glyphicon-star yellow"
-        } else {
-            return "glyphicon glyphicon-star stars";
-        }
-    };
+    console.log($scope.publicaciones);
+    $scope.getpublishedtitle = '';
+    $scope.getpublishedmessage = '';
+  };
+
+
+  $scope.buscar = function() {
+    alert($scope.documentos[0].titulo);
+  };
+
+
+  $scope.marca = function(index, votos) {
+    /*console.log(index, votos);*/
+    if(index <= votos) {
+      return "glyphicon glyphicon-star yellow"
+    }else{
+      return "glyphicon glyphicon-star stars";
+    }
+  };
+
+
 
 });
