cf3b49f951aef15bd3a2fcbd6faba723d2b4ded5
cinthyaw <chloevalery789>
2014-08-09 18:20:41 -0600

    varios ajustes php

--- phpConexion/conexion.php
+++ phpConexion/conexion.php
@@ -1,15 +1,18 @@
 <?php
-error_reporting(2);
+error_reporting(-1);
 ini_set('display_errors', 'On');
 
-$host = "23.229.208.198:3306";
+include_once "funciones.php";
+
+
+$host = "23.229.208.198";
 $usuario = "proyectofinal";
 $password = "Cenfotec2014";
 
 define('__DATABASE__', "proyectofinalweb");
 
 
-$conexion = mysql_pconnect($host, $usuario, $password) or die(mysql_error());
+$conexion = mysqli_connect($host, $usuario, $password, __DATABASE__) or die(mysqli_error());
 
 
 
--- phpConexion/funciones.php
+++ phpConexion/funciones.php
@@ -1,27 +1,10 @@
 <?php
 
-	function ObtenerUsuarios($conexion) {
-
-		$query = "select * from Usuario";
-
-		mysql_select_db(__DATABASE__, $conexion) or die('error'.mysql_error());
-
-		return mysql_query($query, $conexion);
-
-	}
-
-	function ObtenerEntradasBlog(conexion){
-		$query = "select * from Blog";
-
-		mysql_select_db(__DATABASE__, $conexion) or die('error'.mysql_error());
-
-		return mysql_query($query, $conexion);
-	}
-
-
-
-
-
+function BDQuery($query, $conexion) {
+	mysqli_set_charset($conexion, 'utf8');
+	//mysql_select_db(__DATABASE__, $conexion) or die('error'.mysql_error());
+	return mysqli_query($conexion, $query);
+}
 
 
 ?>
