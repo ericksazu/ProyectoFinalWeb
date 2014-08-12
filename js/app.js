e785134440434776576070287bd1b4f5df8e82ba
Manuuel <manuzumu@gmail.com>
2014-08-12 13:47:30 -0600

    Login estable

--- js/app.js
+++ js/app.js
@@ -69,7 +69,6 @@
                     $location.url('/');
                 } else {
 
-                    console.log($rootScope.usuarioLogueado.idRol);
                     var posicion = -1;
                     for (var i = 0; i < $rootScope.roles.length; i++) {
                         if ('/' + $rootScope.roles[i].page == $location.$$path) {
@@ -77,27 +76,12 @@
                         }
                     }
                     if (posicion > -1) {
-                        console.log($rootScope.roles, $rootScope.usuarioLogueado.idRol, posicion);
                         if ($rootScope.roles[posicion].roles.indexOf($rootScope.usuarioLogueado.idRol) == -1) {
                             $location.url('/');
                         }
                     }
                 }
 
-<<<<<<< HEAD
-=======
-          
-          var posicion = -1;
-          for (var i = 0; i < $rootScope.roles.length; i++) {
-            if ('/' + $rootScope.roles[i].page == $location.$$path) {
-              posicion = i;
-            }
-          }
-          if (posicion > -1) {
-
-            if ($rootScope.roles[posicion].roles.indexOf($rootScope.usuarioLogueado.idRol) == -1) {
-              $location.url('/');
->>>>>>> FETCH_HEAD
             }
 
         });
