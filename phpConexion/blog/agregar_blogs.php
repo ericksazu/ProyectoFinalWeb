bb045eff06f008fef2a7605d2bfa9de908d623a4
Erick Sanchez <ericksazu@gmail.com>
2014-08-12 19:07:06 -0600

    documentos php

--- phpConexion/blog/agregar_blogs.php
+++ phpConexion/blog/agregar_blogs.php
@@ -4,37 +4,26 @@
 	include_once "../funciones/blogFunciones.php";
 
 	$data = json_decode(file_get_contents("php://input"));
-	//var_dump($data);
-
-
 
 	$descripcionPublicacion = $data->descripcionPublicacion;
-	//var_dump($descripcionPublicacion);
 	$tema = $data->tema;
 	$fecha = $data->fecha;
 
 
 	$qry = 'INSERT INTO Blog (descripcionPublicacion,fecha,tema) values ("' . $descripcionPublicacion . '","' . $fecha .'","' . $tema .'")';
 
-  /*$query = "insert into Blog (
-				tema,
-				descripcionPublicacion
-			)
-			values (
-				".'tema'.",
-				".'descripcionPublicacion'."
-				)";*/
-
 	Print "Your information has been successfully added to the database.";
 
 	$qry_res = mysqli_query($conexion,$qry);
-	var_dump($qry_res);
+
+	//var_dump($qry_res);
 
 	mysqli_close($conexion);
 
 
 
 	//$resultado = CrearNuevoBlog($conexion);
+	//var_dump($data);
 
 
 ?>
---
+++ phpConexion/documentos/subir_archivos.php
@@ -0,0 +1,10 @@
+<?php
+
+	include_once "../conexion.php";
+	include_once "../funciones/documentosFunciones.php";
+
+
+
+	mysqli_close($conexion);
+
+?>
