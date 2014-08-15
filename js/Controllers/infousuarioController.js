239cbf8908f642a9e1802890d22a627e42ce1359
Manuuel <manuzumu@gmail.com>
2014-08-15 17:51:25 -0600

    Cambio de imagen casi logrado!

--- js/Controllers/infousuarioController.js
+++ js/Controllers/infousuarioController.js
@@ -19,7 +19,7 @@ angular.module('module').controller('infousuarioController', function($scope, $r
             trigger: 'click',
             html: 'true',
             title: "Perfil",
-            content: "<figure class='pull-left center-block' id='circle'><img src='" + $rootScope.usuarioLogueado.foto + "' alt='Foto usuario' class='img-circle' id='fotoperfil'><figcaption><a>Actualizar foto</a><input class='photos' type='file' name='photos[]'></figcaption></figure><div class='pull-right datos'><h5 class='nombre3'>" + $rootScope.usuarioLogueado.nombre + " " + $rootScope.usuarioLogueado.primerApellido + "</h5><p class='rol'>" + cambioRol + "</p><span class='glyphicon glyphicon-star yellow'></span><span class='glyphicon glyphicon-star yellow'></span><span class='glyphicon glyphicon-star yellow'></span><span class='glyphicon glyphicon-star yellow'></span><span class='glyphicon glyphicon-star yellow'></span><a data-toggle='modal' data-target='#cambiarcontrasena'><h5 class='nombre2'>¿Quiere cambiar la contraseña?</h5></a></div>"
+            content: "<figure class='pull-left center-block' id='circle'><img src='phpConexion/login/" + $rootScope.usuarioLogueado.foto + "' alt='Foto usuario' class='img-circle' id='fotoperfil'><a data-toggle='modal' data-target='#cambiarfoto'><p class='actu'> Actualizar foto</p></a></figure><div class='pull-right datos'><h5 class='nombre3'>" + $rootScope.usuarioLogueado.nombre + " " + $rootScope.usuarioLogueado.primerApellido + "</h5><p class='rol'>" + cambioRol + "</p><span class='glyphicon glyphicon-star yellow'></span><span class='glyphicon glyphicon-star yellow'></span><span class='glyphicon glyphicon-star yellow'></span><span class='glyphicon glyphicon-star yellow'></span><span class='glyphicon glyphicon-star yellow'></span><a data-toggle='modal' data-target='#cambiarcontrasena'><h5 class='nombre2'>¿Quiere cambiar la contraseña?</h5></a></div>"
         }
 
     $(popover).popover(options);
@@ -40,6 +40,8 @@ $(".salir").on('click', function(){
   location.reload(true);
 
 }); 
+
+$(":file").filestyle({buttonText: "Cambiar foto"});
 
 
 });
