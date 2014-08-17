ea4eb8aa0a7e9756d65980a219709045cad7f248
Erick Sanchez <ericksazu@gmail.com>
2014-08-17 13:06:07 -0600

    active nav blog, foro, documentos
    Muestra la pestaña seleccionada

--- js/app.js
+++ js/app.js
@@ -131,6 +131,11 @@
         }
     ]);
 
+    $('.nav li a').on('click', function() {
+        $(this).parent().parent().find('.active').removeClass('active');
+        $(this).parent().addClass('active');
+    });
+
 
 
 
