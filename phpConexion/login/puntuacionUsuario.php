147a5b473de76ff0aeeb10c5128015ad0f30a2c0
Manuuel <manuzumu@gmail.com>
2014-08-22 15:36:29 -0600

    Envio de contrasena

---
+++ phpConexion/login/puntuacionUsuario.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once "../conexion.php";
+include_once "../funciones/usuariosFunciones.php";
+
+
+
+$olvido = $_POST['email2'];
+
+// echo 'KILO ' .$olvido; die();
+
+$resultado = "SELECT contrasena FROM Usuario WHERE email = '".$olvido."' ";
+
+$resultado2 = mysqli_query($conexion,$resultado);
+
+$row = mysqli_fetch_assoc($resultado2);
+
+$contrasena = $row['contrasena'];
+
+// echo $contrasena; die();
+//send email containing their password to their email address
+mail($olvido, 'Esta es su contrasena olvidada', "Esta es su contrasena: ".$contrasena."" , 'From: nocontestar@LoginNet.cr');
+
+header('location: ../..#');
+
+mysqli_close($conexion);
+
+?>
--- phpConexion/obtenerEstudiantesForo.php
+++ phpConexion/obtenerEstudiantesForo.php
@@ -3,9 +3,7 @@
 include_once "conexion.php";
 include_once "funciones/forosFunciones.php";
 
-
 $data = json_decode(file_get_contents("php://input"));
-
 
 $resultado = obtenerEstudiantesForo($conexion,$data->idForo);
 
