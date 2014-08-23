5ed9b0985ef3d68c436af50c04205701b3c948b3
Manuuel <manuzumu@gmail.com>
2014-08-23 01:54:35 -0600

    Errorcillos de ortografia

--- phpConexion/login/olvidocontrasena.php
+++ phpConexion/login/olvidocontrasena.php
@@ -17,7 +17,7 @@ $contrasena = $row['contrasena'];
 
 // echo $contrasena; die();
 //send email containing their password to their email address
-mail($olvido, 'Esta es su contraseña olvidada', "Esta es su contraseña: ".$contrasena."" , 'From: nocontestar@LoginNet.cr');
+mail($olvido, 'Recuperación de contraseña olvidada', "Esta es su contraseña: ".$contrasena."" , 'From: nocontestar@LoginNet.cr');
 
 header('location: ../..#');
 
